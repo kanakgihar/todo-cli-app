d02a13fafe054c6f6d790866f013b847e36e5dd5
Kanakgihar <aetherai.ananta@gmail.com>
2026-05-06 12:50:55 +0530

    first commit

---
+++ todo.py
@@ -0,0 +1,44 @@
+tasks = []
+
+def show_menu():
+    print("\n==== TO-DO LIST MENU ====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+def add_task():
+    task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added!")
+
+def view_tasks():
+    if not tasks:
+        print("No tasks available.")
+    else:
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+def delete_task():
+    view_tasks()
+    num = int(input("Enter task number: "))
+    if 1 <= num <= len(tasks):
+        tasks.pop(num-1)
+        print("Task deleted!")
+    else:
+        print("Invalid number")
+
+while True:
+    show_menu()
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        add_task()
+    elif choice == "2":
+        view_tasks()
+    elif choice == "3":
+        delete_task()
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice")
